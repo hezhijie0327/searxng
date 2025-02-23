9b6eec654da1837be491012411f01312170d0c55
Zhijie He <hezhijie0327@hotmail.com>
2025-02-23 15:05:43 +0800

    [fix] fix W0123: Use of eval (eval-used)

--- searx/autocomplete.py
+++ searx/autocomplete.py
@@ -183,8 +183,11 @@ def sogou(query, _lang):
         end_idx = data.rfind("]")
 
         if start_idx != -1 and end_idx != -1:
-            data_list = eval(data[start_idx : end_idx + 1])
-            return data_list[1] if isinstance(data_list, list) else []
+            try:
+                data_list = json.loads(data[start_idx : end_idx + 1])
+                return data_list[1] if isinstance(data_list, list) else []
+            except json.JSONDecodeError:
+                return []
 
     return []
 
