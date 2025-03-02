11b25e5c209a7ad2bd901094314b6416cb4feac0
Zhijie He <hezhijie0327@hotmail.com>
2025-03-02 09:19:21 +0800

    Update and rename 360so.py to 360search.py

--- searx/engines/360search.py
+++ searx/engines/360search.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # pylint: disable=invalid-name
-"""360So search engine for searxng"""
+"""360Search search engine for searxng"""
 
 from urllib.parse import urlencode
 from lxml import html
