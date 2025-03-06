5c21960148623e0b20cfc6617256b93e643bdd63
Zhijie He <hezhijie0327@hotmail.com>
2025-03-06 18:53:35 +0800

    Update baidu_images.py

--- searx/engines/baidu_images.py
+++ searx/engines/baidu_images.py
@@ -25,7 +25,7 @@ base_url = "https://image.baidu.com"
 
 
 def request(query, params):
-    query_params = {"tn": "resultjson_com", "word": query, "pn": params["pageno"], "rn": 30}
+    query_params = {"tn": "resultjson_com", "word": query, "pn": (params["pageno"] - 1) * results_per_page, "rn": results_per_page}
 
     params["url"] = f"{base_url}/search/acjson?{urlencode(query_params)}"
     return params
@@ -33,26 +33,25 @@ def request(query, params):
 
 def response(resp):
     try:
-        clean_text = resp.text.replace("\\", "\\\\")
-        data = json.loads(clean_text, strict=False)
+        data = json.loads(resp.text, strict=False)
     except Exception as e:
         raise SearxEngineAPIException(f"Invalid response: {e}") from e
     results = []
 
     if "data" in data:
         for item in data["data"]:
-            replace_url = item.get("replaceUrl", [])
-            from_url = replace_url[0].get("FromURL", "") if replace_url else ""
-            img_src = replace_url[0].get("ObjURL", "") if replace_url else ""
+            replace_url = item.get("replaceUrl", [{}])[0]
+            from_url = replace_url.get("FromURL", "").replace("\\/", "/")
+            img_src = replace_url.get("ObjURL", "").replace("\\/", "/")
 
             results.append(
                 {
                     "template": "images.html",
-                    "url": from_url.replace("\\/", "/"),
+                    "url": from_url,
                     "thumbnail_src": item.get("thumbURL", ""),
-                    "img_src": img_src.replace("\\/", "/"),
-                    "content": item.get("fromPageTitleEnc", ""),
-                    "title": item.get("fromPageTitle", ""),
+                    "img_src": img_src,
+                    "content": html_to_text(item.get("fromPageTitleEnc", "")),
+                    "title": html_to_text(item.get("fromPageTitle", "")),
                     "source": item.get("fromURLHost", ""),
                 }
             )
