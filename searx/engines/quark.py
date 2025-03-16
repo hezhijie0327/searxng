026b287801d026473e49c778134df175fa9bbf71
Zhijie He <hezhijie0327@hotmail.com>
2025-03-16 13:51:10 +0800

    Update quark.py

--- searx/engines/quark.py
+++ searx/engines/quark.py
@@ -167,7 +167,12 @@ def parse_ai_page(data):
             else str(item.get('content'))
         )
         results.append(
-            {"title": html_to_text(item.get('title')), "url": item.get('url'), "content": html_to_text(content)}
+            {
+                "title": html_to_text(item.get('title')),
+                "url": item.get('url'),
+                "content": html_to_text(content),
+                "published_date": datetime.fromtimestamp(int(item.get('source', {}).get('time'))),
+            }
         )
     return results
 
@@ -247,6 +252,7 @@ def parse_ss_note(data):
         "title": html_to_text(data.get('title', {}).get('content')),
         "url": data.get('source', {}).get('dest_url'),
         "content": html_to_text(data.get('summary', {}).get('content')),
+        "published_date": datetime.fromtimestamp(int(data.get('source', {}).get('time')))
     }
 
 
@@ -255,6 +261,7 @@ def parse_ss_pic_text(data):
         "title": html_to_text(data.get('titleProps', {}).get('content')),
         "url": data.get('sourceProps', {}).get('dest_url'),
         "content": html_to_text(data.get('summaryProps', {}).get('content')),
+        "published_date": datetime.fromtimestamp(int(data.get('sourceProps', {}).get('time')))
     }
 
 
