c30eb5663920dd941216b3534bf658a5438d587e
Zhijie He <hezhijie0327@hotmail.com>
2025-03-16 08:08:39 +0800

    Update quark.py

--- searx/engines/quark.py
+++ searx/engines/quark.py
@@ -72,95 +72,137 @@ def response(resp):
         initial_data = data.get('data', {}).get('initialData', {})
         extra_data = data.get('extraData', {})
 
-        sc = extra_data.get('sc')
+        source_category = extra_data.get('sc')
+        source_category_parsers = {
+            addition: parse_addition,
+            ai_page: parse_ai_page,
+            baike_sc: parse_baike,
+            finance_shuidi: parse_finance_shuidi,
+            nature_result: parse_nature_result,
+            news_uchq: parse_news_uchq,
+            ss_note: parse_ss_note,
+            ss_pic: parse_ss_pic_text,
+            ss_text: parse_ss_pic_text,
+            travel_dest_overview: parse_travel_dest_overview,
+            travel_ranking_list: parse_travel_ranking_list,
+        }
 
-        title, content, url = None, None, None
-
-        # General
-        if sc == 'ss_pic' or sc == 'ss_text':
-            title = initial_data.get('titleProps', {}).get('content')
-            content = initial_data.get('summaryProps', {}).get('content')
-            url = initial_data.get('sourceProps', {}).get('dest_url')
-
-        if sc == 'ss_note':
-            title = initial_data.get('title', {}).get('content')
-            content = initial_data.get('summary', {}).get('content')
-            url = initial_data.get('source', {}).get('dest_url')
-
-        if sc == 'nature_result':
-            title = initial_data.get('title')
-            content = initial_data.get('desc')
-            url = initial_data.get('url')
-
-        if sc == 'addition':
-            title = initial_data.get('title', {}).get('content')
-            content = initial_data.get('summary', {}).get('content')
-            url = initial_data.get('source', {}).get('url')
-
-        # Baike (Wiki)
-        if sc == 'baike_sc':
-            title = initial_data.get('data', {}).get('title')
-            content = initial_data.get('data', {}).get('abstract')
-            url = initial_data.get('data', {}).get('url')
-
-        # Shuidi (Company DB)
-        if sc == 'finance_shuidi':
-            title = initial_data.get('company_name')
-            content = " | ".join(
-                filter(
-                    None,
-                    [
-                        initial_data.get('establish_time'),
-                        initial_data.get('company_status'),
-                        initial_data.get('controled_type'),
-                        initial_data.get('company_type'),
-                        initial_data.get('capital'),
-                        initial_data.get('address'),
-                        initial_data.get('business_scope'),
-                    ],
-                )
-            )
-            url = initial_data.get('title_url')
-
-        # Travel
-        if sc == 'travel_dest_overview':
-            title = initial_data.get('strong', {}).get('title')
-            content = initial_data.get('strong', {}).get('baike_text')
-            url = initial_data.get('strong', {}).get('baike_url')
-
-        if sc == 'travel_ranking_list':
-            title = initial_data.get('title', {}).get('text')
-            content = initial_data.get('title', {}).get('title_tag')
-            url = initial_data.get('title', {}).get('url')
-
-        # News
-        if sc == 'news_uchq':
-            for item in initial_data.get('feed', []):
-                results.append(
-                    {
-                        "title": html_to_text(item.get('title')),
-                        "url": item.get('url'),
-                        "content": html_to_text(item.get('summary')),
-                    }
-                )
-            # skip dups append for news_uchq
-            continue
-
-        # AI Contents
-        if sc == 'ai_page':
-            for item in initial_data.get('list', []):
-                content = (
-                    " | ".join(map(str, item.get('content', [])))
-                    if isinstance(item.get('content'), list)
-                    else str(item.get('content'))
-                )
-                results.append(
-                    {"title": html_to_text(item.get('title')), "url": item.get('url'), "content": html_to_text(content)}
-                )
-            # skip dups append for ai_page
-            continue
-
-        if title and content:
-            results.append({"title": html_to_text(title), "url": url, "content": html_to_text(content)})
+        parsers = source_category_parsers.get(source_category)
+        if parsers:
+            parsed_results = parsers(initial_data)
+            if isinstance(parsed_results, list):
+                # Extend if the result is a list
+                results.extend(parsed_results)
+            else:
+                # Append if it's a single result
+                results.append(parsed_results)
 
     return results
+
+
+def parse_addition(data):
+    return {
+        "title": html_to_text(data.get('title', {}).get('content')),
+        "url": data.get('source', {}).get('url'),
+        "content": html_to_text(data.get('summary', {}).get('content'))
+    }
+
+
+def parse_ai_page(data):
+    results = []
+    for item in data.get('list', []):
+        content = (
+            " | ".join(map(str, item.get('content', [])))
+            if isinstance(item.get('content'), list)
+            else str(item.get('content'))
+        )
+        results.append(
+            {
+                "title": html_to_text(item.get('title')),
+                "url": item.get('url'),
+                "content": html_to_text(content)}
+        )
+    return results
+
+
+def parse_baike(data):
+    return {
+        "title": html_to_text(data.get('data', {}).get('title')),
+        "url": data.get('data', {}).get('url'),
+        "content": html_to_text(data.get('data', {}).get('abstract'))
+    }
+
+
+def parse_finance_shuidi(data):
+    content = " | ".join(
+        filter(
+            None,
+            [
+                data.get('establish_time'),
+                data.get('company_status'),
+                data.get('controled_type'),
+                data.get('company_type'),
+                data.get('capital'),
+                data.get('address'),
+                data.get('business_scope'),
+            ],
+        )
+    )
+    return {
+        "title": html_to_text(data.get('company_name')),
+        "url": data.get('title_url'),
+        "content": html_to_text(content)
+    }
+
+
+def parse_nature_result(data):
+    return {
+        "title": html_to_text(data.get('title')),
+        "url": data.get('url'),
+        "content": html_to_text(data.get('desc'))
+    }
+
+
+def parse_news_uchq(data):
+    results = []
+    for item in data.get('feed', []):
+        results.append(
+            {
+                "title": html_to_text(item.get('title')),
+                "url": item.get('url'),
+                "content": html_to_text(item.get('summary')),
+            }
+        )
+    return results
+
+
+def parse_ss_note(data):
+    return {
+        "title": html_to_text(data.get('title', {}).get('content')),
+        "url": data.get('source', {}).get('dest_url'),
+        "content": html_to_text(data.get('summary', {}).get('content'))
+    }
+
+
+def parse_ss_pic_text(data):
+    return {
+        "title": html_to_text(data.get('titleProps', {}).get('content')),
+        "url": data.get('sourceProps', {}).get('dest_url'),
+        "content": html_to_text(data.get('summaryProps', {}).get('content'))
+    }
+
+
+def parse_travel_dest_overview(data):
+    return {
+        "title": html_to_text(data.get('strong', {}).get('title')),
+        "url": data.get('strong', {}).get('baike_url'),
+        "content": html_to_text(data.get('strong', {}).get('baike_text'))
+    }
+
+
+def parse_travel_ranking_list(data):
+    return {
+        "title": html_to_text(data.get('title', {}).get('text')),
+        "url": data.get('title', {}).get('url'),
+        "content": html_to_text(data.get('title', {}).get('title_tag'))
+    }
