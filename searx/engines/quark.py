82b24c7b3ca6326b848d0a35585da4a52b168ef0
Zhijie He <hezhijie0327@hotmail.com>
2025-03-16 11:26:34 +0800

    Update quark.py

--- searx/engines/quark.py
+++ searx/engines/quark.py
@@ -7,7 +7,7 @@ import re
 import json
 
 from searx.utils import html_to_text, searx_useragent
-from searx.exceptions import SearxEngineCaptchaException
+from searx.exceptions import SearxEngineAPIException, SearxEngineCaptchaException
 
 # Metadata
 about = {
@@ -20,13 +20,13 @@ about = {
 }
 
 # Engine Configuration
-categories = ["general"]
+categories = []
 paging = True
+results_per_page = 10
+
+quark_category = 'general'
+
 time_range_support = True
-
-# Base URL
-base_url = "https://m.quark.cn"
-
 time_range_dict = {'day': '4', 'week': '3', 'month': '2', 'year': '1'}
 
 
@@ -46,13 +46,40 @@ def is_alibaba_captcha(html):
         raise SearxEngineCaptchaException(suspended_time=900, message=f"Alibaba CAPTCHA: {captcha_url}")
 
 
-def request(query, params):
-    query_params = {"q": query, "layout": "html", "page": params["pageno"]}
+def init(_):
+    if quark_category not in ('general', 'images'):
+        raise SearxEngineAPIException(f"Unsupported category: {quark_category}")
 
-    if time_range_dict.get(params['time_range']):
+
+def request(query, params):
+    page_num = params["pageno"]
+
+    category_config = {
+        'general': {
+            'endpoint': 'https://m.quark.cn/s',
+            'params': {
+                "q": query,
+                "layout": "html",
+                "page":  page_num,
+            },
+        },
+        'images': {
+            'endpoint': 'https://vt.sm.cn/api/pic/list',
+            'params': {
+                "query": query,
+                "limit": results_per_page,
+                "start": (page_num - 1) * results_per_page,
+            },
+        },
+    }
+
+    query_params = category_config[quark_category]['params']
+    query_url = category_config[quark_category]['endpoint']
+
+    if time_range_dict.get(params['time_range']) and quark_category == 'general':
         query_params["tl_request"] = time_range_dict.get(params['time_range'])
 
-    params["url"] = f"{base_url}/s?{urlencode(query_params)}"
+    params["url"] = f"{query_url}?{urlencode(query_params)}"
     params["headers"] = {
         "User-Agent": searx_useragent(),
     }
@@ -61,45 +88,67 @@ def request(query, params):
 
 def response(resp):
     results = []
-    html_content = resp.text
+    text = resp.text
 
-    is_alibaba_captcha(html_content)
+    is_alibaba_captcha(text)
 
-    pattern = r'<script\s+type="application/json"\s+id="s-data-[^"]+"\s+data-used-by="hydrate">(.*?)</script>'
-    matches = re.findall(pattern, html_content, re.DOTALL)
+    if quark_category == 'images':
+        data = json.loads(text)
+        for item in data.get('data', {}).get('hit', {}).get('imgInfo', {}).get('item', []):
+            width = item.get("width")
+            height = item.get("height")
+            published_date = datetime.fromtimestamp(int(item.get("publish_time")))
+    
+            results.append(
+                {
+                    "template": "images.html",
+                    "url": item.get("imgUrl"),
+                    "thumbnail_src": item.get("img"),
+                    "img_src": item.get("bigPicUrl"),
+                    "title": item.get("title"),
+                    "source": item.get("site"),
+                    "resolution": f"{width} x {height}",
+                    "publishedDate": published_date,
+                }
+            )
+        return results
 
-    for match in matches:
-        data = json.loads(match)
-        initial_data = data.get('data', {}).get('initialData', {})
-        extra_data = data.get('extraData', {})
-
-        source_category = extra_data.get('sc')
-        source_category_parsers = {
-            'addition': parse_addition,
-            'ai_page': parse_ai_page,
-            'baike_sc': parse_baike_sc,
-            'finance_shuidi': parse_finance_shuidi,
-            'life_show_general_image': parse_life_show_general_image,
-            'nature_result': parse_nature_result,
-            'news_uchq': parse_news_uchq,
-            'ss_note': parse_ss_note,
-            'ss_pic': parse_ss_pic_text,
-            'ss_text': parse_ss_pic_text,
-            'travel_dest_overview': parse_travel_dest_overview,
-            'travel_ranking_list': parse_travel_ranking_list,
-        }
-
-        parsers = source_category_parsers.get(source_category)
-        if parsers:
-            parsed_results = parsers(initial_data)
-            if isinstance(parsed_results, list):
-                # Extend if the result is a list
-                results.extend(parsed_results)
-            else:
-                # Append if it's a single result
-                results.append(parsed_results)
-
-    return results
+    if quark_category == 'general':
+        pattern = r'<script\s+type="application/json"\s+id="s-data-[^"]+"\s+data-used-by="hydrate">(.*?)</script>'
+        matches = re.findall(pattern, text, re.DOTALL)
+    
+        for match in matches:
+            data = json.loads(match)
+            initial_data = data.get('data', {}).get('initialData', {})
+            extra_data = data.get('extraData', {})
+    
+            source_category = extra_data.get('sc')
+            source_category_parsers = {
+                'addition': parse_addition,
+                'ai_page': parse_ai_page,
+                'baike_sc': parse_baike_sc,
+                'finance_shuidi': parse_finance_shuidi,
+                'life_show_general_image': parse_life_show_general_image,
+                'nature_result': parse_nature_result,
+                'news_uchq': parse_news_uchq,
+                'ss_note': parse_ss_note,
+                'ss_pic': parse_ss_pic_text,
+                'ss_text': parse_ss_pic_text,
+                'travel_dest_overview': parse_travel_dest_overview,
+                'travel_ranking_list': parse_travel_ranking_list,
+            }
+    
+            parsers = source_category_parsers.get(source_category)
+            if parsers:
+                parsed_results = parsers(initial_data)
+                if isinstance(parsed_results, list):
+                    # Extend if the result is a list
+                    results.extend(parsed_results)
+                else:
+                    # Append if it's a single result
+                    results.append(parsed_results)
+    
+        return results
 
 
 def parse_addition(data):
