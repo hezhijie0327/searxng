216eb377665a79465e13a4800d0a68da152b962f
Zhijie He <hezhijie0327@hotmail.com>
2025-03-20 06:23:05 +0800

    Update niconico.py

--- searx/engines/niconico.py
+++ searx/engines/niconico.py
@@ -1,13 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """Niconico search engine for searxng"""
 
-import time
-
 from urllib.parse import urlencode
 from datetime import datetime, timedelta
 from lxml import html
 
-from searx.utils import extract_text
+from searx.utils import eval_xpath_getindex, eval_xpath_list, eval_xpath, extract_text
 
 about = {
     "website": "https://www.nicovideo.jp/",
@@ -22,20 +20,27 @@ categories = ["videos"]
 paging = True
 
 time_range_support = True
-time_range_dict = {"day": 86400, "week": 604800, "month": 2592000, "year": 31536000}
+time_range_dict = {"day": 1, "week": 7, "month": 30, "year": 365}
 
 base_url = "https://www.nicovideo.jp"
+embed_url = "https://embed.nicovideo.jp"
+
+results_xpath = '//li[@data-video-item]'
+url_xpath = './/a[@class="itemThumbWrap"]/@href'
+video_length_xpath = './/span[@class="videoLength"]'
+upload_time_xpath = './/p[@class="itemTime"]//span[@class="time"]/text()'
+title_xpath = './/p[@class="itemTitle"]/a'
+content_xpath = './/p[@class="itemDescription"]/@title'
+thumbnail_xpath = './/img[@class="thumb"]/@src'
 
 
 def request(query, params):
     query_params = {"page": params['pageno']}
 
     if time_range_dict.get(params['time_range']):
-        now = int(time.time())
-        past = now - time_range_dict[params["time_range"]]
-
-        query_params['end'] = time.strftime('%Y-%m-%d', time.gmtime(now))
-        query_params['start'] = time.strftime('%Y-%m-%d', time.gmtime(past))
+        time_diff_days = time_range_dict[params['time_range']]
+        start_date = datetime.now() - timedelta(days=time_diff_days)
+        query_params['start'] = start_date.strftime('%Y-%m-%d')
 
     params['url'] = f"{base_url}/search/{query}?{urlencode(query_params)}"
     return params
@@ -45,16 +50,16 @@ def response(resp):
     results = []
     dom = html.fromstring(resp.text)
 
-    for item in dom.xpath('//li[@data-video-item]'):
-        relative_url = extract_text(item.xpath('.//a[@class="itemThumbWrap"]/@href')[0])
+    for item in eval_xpath_list(dom, results_xpath):
+        relative_url = eval_xpath_getindex(item, url_xpath, 0)
         video_id = relative_url.rsplit('?', maxsplit=1)[0].split('/')[-1]
 
         url = f"{base_url}/watch/{video_id}"
-        iframe_src = f"https://embed.nicovideo.jp/watch/{video_id}"
+        iframe_src = f"{embed_url}/watch/{video_id}"
 
         length = None
-        video_length = extract_text(item.xpath('.//span[@class="videoLength"]'))
-        if video_length:
+        video_length = eval_xpath_getindex(item, video_length_xpath, 0)
+        if len(video_length) > 0:
             try:
                 timediff = datetime.strptime(video_length, "%M:%S")
                 length = timedelta(minutes=timediff.minute, seconds=timediff.second)
@@ -62,8 +67,8 @@ def response(resp):
                 pass
 
         published_date = None
-        upload_time = extract_text(item.xpath('.//p[@class="itemTime"]//span[@class="time"]/text()'))
-        if upload_time:
+        upload_time = eval_xpath_getindex(item, upload_time_xpath, 0)
+        if len(upload_time) > 0:
             try:
                 published_date = datetime.strptime(upload_time, "%Y/%m/%d %H:%M")
             except ValueError:
@@ -72,11 +77,11 @@ def response(resp):
         results.append(
             {
                 'template': 'videos.html',
-                'title': extract_text(item.xpath('.//p[@class="itemTitle"]/a')),
-                'content': extract_text(item.xpath('.//p[@class="itemDescription"]/@title')[0]),
+                'title': extract_text(eval_xpath(item, title_xpath)),
+                'content': eval_xpath_getindex(item, content_xpath, 0),
                 'url': url,
                 "iframe_src": iframe_src,
-                'thumbnail': extract_text(item.xpath('.//img[@class="thumb"]/@src')[0]),
+                'thumbnail': eval_xpath_getindex(item, thumbnail_xpath, 0),
                 'length': length,
                 "publishedDate": published_date,
             }
