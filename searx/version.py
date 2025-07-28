07ad5308c5e3e7b22da6d3fac183ec9c3792e762
Zhijie He <hezhijie0327@hotmail.com>
2025-07-28 17:31:37 +0800

    Update version.py

--- searx/version.py
+++ searx/version.py
@@ -91,16 +91,14 @@ def get_information():
     git_url = GIT_URL
     git_branch = GIT_BRANCH
 
-    """
-    try:
-        version_string, version_tag, docker_tag = get_git_version()
-    except subprocess.CalledProcessError as ex:
-        logger.error("Error while getting the version: %s", ex.stderr)
-    try:
-        git_url, git_branch = get_git_url_and_branch()
-    except subprocess.CalledProcessError as ex:
-        logger.error("Error while getting the git URL & branch: %s", ex.stderr)
-    """
+    # try:
+    #     version_string, version_tag, docker_tag = get_git_version()
+    # except subprocess.CalledProcessError as ex:
+    #     logger.error("Error while getting the version: %s", ex.stderr)
+    # try:
+    #     git_url, git_branch = get_git_url_and_branch()
+    # except subprocess.CalledProcessError as ex:
+    #     logger.error("Error while getting the git URL & branch: %s", ex.stderr)
 
     return version_string, version_tag, docker_tag, git_url, git_branch
 
