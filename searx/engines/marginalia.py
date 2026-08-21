260cb01dac8d148fc8a1abc1de53caae1cda29df
Zhijie He <hezhijie0327@hotmail.com>
2026-08-21 09:29:21 +0800

    Update marginalia.py

--- searx/engines/marginalia.py
+++ searx/engines/marginalia.py
@@ -170,10 +170,14 @@ def init(_: dict[str, t.Any]):
         logger.error("missing api_key: see https://about.marginalia-search.com/article/api")
         return False
 
-    if filter_name:
+    """
+    if api_key == "public":
+        logger.error("invalid api_key (%s): see https://about.marginalia-search.com/article/api", api_key)
+    elif filter_name:
         filter_names: list[str] = _get_filter_names()
         if filter_name not in filter_names:
             logger.error(f"invalid value for filter_name: '{filter_name}'")
             return False
+    """
 
     return True
